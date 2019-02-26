5dec3ce530753323f6c60f9305810c5ab7a6e022
Makwana <mitesh121999@gmail.com>
2019-02-26 18:32:16 +0530

    add song

--- app/src/main/java/com/example/demoopengl/MyGlActivity.java
+++ app/src/main/java/com/example/demoopengl/MyGlActivity.java
@@ -1,14 +1,17 @@
 package com.example.demoopengl;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 public class MyGlActivity extends Activity {
 
     //use Gl surface view
     private GLSurfaceView glSurfaceView;
+    private MediaPlayer mp;
 
     //call back when the activity is started,and initialize the view
 
@@ -19,6 +22,15 @@ public class MyGlActivity extends Activity {
         glSurfaceView = new GLSurfaceView( this ); //allocate Gl surface
         glSurfaceView.setRenderer( new MyGlRenderer( this ) );//use custom renderer
         this.setContentView( glSurfaceView );
+
+        // song
+        glSurfaceView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp=MediaPlayer.create( MyGlActivity.this,R.raw.harry_potter_theme );
+                mp.start();
+            }
+        } );
     }
 
     //call back when the activity is going to background
@@ -26,6 +38,8 @@ public class MyGlActivity extends Activity {
     protected  void onPause(){
         super.onPause();
         glSurfaceView.onPause();
+
+        mp.release();
     }
 
     //call back after the onpause
